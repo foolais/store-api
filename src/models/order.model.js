09ae2eba5d3cf490ee6648ed05c3d6a8c941422a
wahyu.esya17 <wahyu.esya17@gmail.com>
2024-04-07 22:36:37 +0700

    feat(Order) add notes model

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -39,6 +39,10 @@ const orderSchema = new mongoose.Schema({
         type: Boolean,
         required: true,
         default: false
+      },
+      notes: {
+        type: String,
+        default: ''
       }
     }
   ],
